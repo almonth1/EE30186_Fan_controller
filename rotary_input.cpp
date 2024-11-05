1c4bf7981590a02e56903b37b3c99ea55859cd60
almonth1 <almonth@yahoo.ca>
2024-11-05 11:51:06 +0000

    rotary input

---
+++ rotary_input.cpp
@@ -0,0 +1,27 @@
+#include "rotary_input.h"
+
+
+// bool currentB;
+// bool prevB = bSignal.read();
+// bool prevClk = aClock.read();
+// bool currentClk = aClock.read();
+        
+        
+void Rotary_Input(){
+// if(currentClk != prevClk)
+// {
+// currentB = bSignal.read();
+//     if(currentB == currentClk)   
+//     {
+//     led_ex = true;
+    
+//     }
+//     else {
+    
+//     led_ex = false;
+//     }
+//     prevB = currentB;
+//     prevClk = currentClk;
+//     wait_us(1000);
+// }
+}
