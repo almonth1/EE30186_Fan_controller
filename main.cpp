67e35021c12c931a22d7dafdbef27ad96aa308fa
almonth1 <almonth@yahoo.ca>
2024-12-02 13:27:20 +0000

    ignore 2

--- main.cpp
+++ main.cpp
@@ -51,14 +51,11 @@ int Button_Mode = 0;
 int target_value;
 int current_temp;
 int target_temp;
-<<<<<<< HEAD
 float previous_fan_rpm = 0;
 float previous_duty_cycle = -1;
 int previous_target_rpm = -1;
 float previous_temp = -1.0;
 int previous_target_temp = -1;
-=======
->>>>>>> parent of c787c3c (henry update)
 float pwm_period;
 bool init_low_PID = true;
 bool init_high_PID = true;
@@ -118,7 +115,6 @@ int main() {
             wait_us(500000);
 
 
-<<<<<<< HEAD
                     target_value = RotaryInput_GetPosition();  // Get the current encoder position
                     duty_cycle = target_value/100.0;
                     if (target_value > 0 && fanrpm <= 10.0){
@@ -127,135 +123,22 @@ int main() {
                     else {
                      FanPWM.write(duty_cycle);
                     }
-                   
-                    if ( std::chrono::duration_cast<std::chrono::milliseconds>(
-                            printTimer.elapsed_time()) >= 1000ms) {
-                            printf("Average RPM: %g\n", fanrpm);
-                            //printf("PWM duty %g\n", pid_output);
-                            //printf("rotary position: %d\n", target_value);
+
+                    if (fanrpm != previous_fan_rpm || duty_cycle != previous_duty_cycle) {
                             lcd.cls();
                             wait_us(2000);
                             lcd.locate(0, 0);  // Move the cursor to the first row
-                            lcd.printf("RPM: %.1f", fanrpm);  // Print current fan RPM (add trailing spaces to clear leftovers)
-                            lcd.locate(0, 1);  // Move the cursor to the first row
-                            lcd.printf("Duty Cycle: %.1f", duty_cycle);  //
-                            printTimer.reset();
-                             }
+                            lcd.printf("Current RPM:%0.1f", fanrpm);  // Print current fan RPM (add trailing spaces to clear leftovers)
 
+                            lcd.locate(0, 1);  // Move the cursor to the second row
+                            lcd.printf("Duty Cycle: %.1f", duty_cycle);
+
+                            // Update the previous values
+                            previous_fan_rpm = fanrpm;
+                            previous_duty_cycle = duty_cycle; 
+                    }                
                     break;
-=======
-       
-        // // Check if the button was pressed to change the mode
-        // if (WasButtonPressed()) {
-        //     Button_Mode++;     
-        //     if (Button_Mode > 5) {
-        //         Button_Mode = 0;  // Wrap around to mode 1 after mode 3
-        //     }
-        //     switch (Button_Mode){
-        //     case 0:
-        //         // Mode 0: Open Loop Speed Control
-        //         Init_Calculate_Fan_RPM();
-        //         Detach_Low_Speed_Pulses();
-        //         sevseg.SevSegWriteOnes(seg_zero);
-        //         sevseg.SevSegWriteTens(seg_zero);
-        //         lcd.cls();
-        //         wait_us(2000);
-        //         lcd.locate(0, 0);  
-        //         lcd.printf("Open Loop Mode"); 
-        //         wait_us(1000000);
-        //         break;
-        //     case 1:
-        //         // Mode 1: PID Speed Control
-        //         previous_duty_cycle = -1;
-        //         if (target_value >= 1000){
-        //             Init_PID_Controller(pid_highspeed_ptr, highspeed_controller_params);
-        //         }
-        //         else{
-        //             Init_PID_Controller(pid_lowspeed_ptr, lowspeed_controller_params);
-        //         }
-        //         printf("target value = %d\n", target_value);
-        //         sevseg.SevSegWriteOnes(seg_one);
-        //         sevseg.SevSegWriteTens(seg_zero);
-        //         lcd.cls();
-        //         wait_us(2000);
-        //         lcd.locate(0, 0);  
-        //         lcd.printf("Speed Control"); 
-        //         lcd.locate(0, 1);  
-        //         lcd.printf("Mode");
-        //         wait_us(1000000);
-        //         break;
-        //     case 2:
-        //         // Mode 2: PID Temperature Limiting  
-        //         previous_target_rpm = -1;     
-        //         Init_PID_Controller(pid_temp_ptr, temp_controller_params);
-        //         sevseg.SevSegWriteOnes(seg_two);
-        //         sevseg.SevSegWriteTens(seg_zero);
-        //         lcd.cls();
-        //         wait_us(2000);
-        //         lcd.locate(0, 0);  
-        //         lcd.printf("Temp Limit Mode"); 
-        //         wait_us(1000000);
-        //         current_temp = Read_Temperature();
-        //         break;
-        //     case 3:
-        //         // Mode 3: Timer display (e.g., TIMER mode)
-        //         previous_target_temp = -1;
-        //         sevseg.SevSegWriteOnes(seg_three);
-        //         sevseg.SevSegWriteTens(seg_zero);
-        //         lcd.cls();
-        //         wait_us(10000);
-        //         lcd.locate(0, 0);  
-        //         lcd.printf("Max Speed Timer Mode");  //
-        //         break;
-        //     case 4:
-        //     if (target_value == 0) {
-        //         Button_Mode++;
-        //         FanPWM.write(0);
-        //         Init_Low_Speed_Pulses();
-        //         sevseg.SevSegWriteOnes(seg_five);
-        //         sevseg.SevSegWriteTens(seg_zero);
-        //         lcd.cls();
-        //         wait_us(10000);
-        //         lcd.locate(0, 0);  
-        //         lcd.printf("Case 5");
-        //     }
-        //         break;
-        //     case 5:
-        //         FanPWM.write(0);
-        //         Init_Low_Speed_Pulses();
-        //         sevseg.SevSegWriteOnes(seg_five);
-        //         sevseg.SevSegWriteTens(seg_zero);
-        //         lcd.cls();
-        //         wait_us(10000);
-        //         lcd.locate(0, 0);  
-        //         lcd.printf("Case 5");
-        //         break;
-        //     default:
-        //         // Mode 0: Open Loop Speed Control
-        //         break;
-        // }
 
-        //     Init_Rotary_Input(Button_Mode);
-        //     printf("Switched to mode %d\n", Button_Mode);
-        // }
-        //     switch (Button_Mode) {
-        //         case 0:      
-
-        //             target_value = RotaryInput_GetPosition();  // Get the current encoder position
-        //             duty_cycle = target_value/100.0;
-        //             if (target_value > 0 && fanrpm <= 10.0){
-        //                 Kick_Start_pulse(fanrpm);
-        //             }
-        //             else {
-        //              FanPWM.write(duty_cycle);
-        //             }
-
-        //             if (fanrpm != previous_fan_rpm || duty_cycle != previous_duty_cycle) {
-        //                     lcd.cls();
-        //                     wait_us(2000);
-        //                     lcd.locate(0, 0);  // Move the cursor to the first row
-        //                     lcd.printf("Current RPM:%0.1f", fanrpm);  // Print current fan RPM (add trailing spaces to clear leftovers)
->>>>>>> parent of cf7a566 (blah 2)
 
         //                     lcd.locate(0, 1);  // Move the cursor to the second row
         //                     lcd.printf("Duty Cycle: %.1f", duty_cycle);
@@ -294,7 +177,7 @@ int main() {
         //                     //FanPWM.period(0.002);
         //                     FanPWM.write(duty_cycle);
                             
-<<<<<<< HEAD
+
                         }
                         else{
                             init_high_PID = true;
@@ -305,62 +188,24 @@ int main() {
                             duty_cycle = PID_Control(pid_lowspeed_ptr, target_value, fanrpm, true);
                             FanPWM.write(duty_cycle);                    
                         }
-                        }          
-                        
-                        if ( std::chrono::duration_cast<std::chrono::milliseconds>(
-                            printTimer.elapsed_time()) >= 500ms) {
-                            printf("Average RPM: %g\n", fanrpm);
-                            printf("Average Duty: %g\n", duty_cycle);
-                            //printf("PWM duty %g\n", pid_output);
-                            //printf("rotary position: %d\n", target_value);
+                        }
 
+                        if (fanrpm != previous_fan_rpm || target_value != previous_target_rpm) {
                             lcd.cls();
                             wait_us(2000);
                             lcd.locate(0, 0);  // Move the cursor to the first row
                             lcd.printf("Current RPM:%0.0f", fanrpm);  // Print current fan RPM (add trailing spaces to clear leftovers)
 
                             lcd.locate(0, 1);  // Move the cursor to the second row
-
                             lcd.printf("Desired RPM:%d", target_value);  // Print the target RPM (add trailing spaces to clear leftovers)
 
                             // Update the previous values
                             previous_fan_rpm = fanrpm;
                             previous_target_rpm = target_value;
                         }        
-                            lcd.printf("Desired RPM:%d", target_value);  // Print the PWM duty cycle (add trailing spaces to clear leftovers)
-                            printTimer.reset();
-                             }
                     #endif
                     break;
-=======
-        //                 }
-        //                 else{
-        //                     init_high_PID = true;
-        //                     if (init_low_PID) {
-        //                         Init_PID_Controller(pid_lowspeed_ptr, lowspeed_controller_params);
-        //                         init_low_PID = false;
-        //                     }
-        //                     duty_cycle = PID_Control(pid_lowspeed_ptr, target_value, fanrpm, true);
-        //                     FanPWM.write(duty_cycle);                    
-        //                 }
-        //                 }
 
-        //                 if (fanrpm != previous_fan_rpm || target_value != previous_target_rpm) {
-        //                     lcd.cls();
-        //                     wait_us(2000);
-        //                     lcd.locate(0, 0);  // Move the cursor to the first row
-        //                     lcd.printf("Current RPM:%0.0f", fanrpm);  // Print current fan RPM (add trailing spaces to clear leftovers)
-
-        //                     lcd.locate(0, 1);  // Move the cursor to the second row
-        //                     lcd.printf("Desired RPM:%d", target_value);  // Print the target RPM (add trailing spaces to clear leftovers)
-
-        //                     // Update the previous values
-        //                     previous_fan_rpm = fanrpm;
-        //                     previous_target_rpm = target_value;
-        //                 }          
-        //             #endif
-        //             break;
->>>>>>> parent of cf7a566 (blah 2)
 
         //         case 2:
         //             // Mode 2: PID Temperature Limiting
@@ -380,14 +225,8 @@ int main() {
         //              FanPWM.write(duty_cycle);
         //             }
                 
-<<<<<<< HEAD
-<<<<<<< HEAD
 
                     if (current_temp != previous_temp || target_temp != previous_target_rpm) {
-=======
-                    if ( std::chrono::duration_cast<std::chrono::milliseconds>(
-                            printTimer.elapsed_time()) >= 1000ms) {
->>>>>>> parent of c787c3c (henry update)
                             lcd.cls();
                             wait_us(2000);
                             lcd.locate(0, 0);  // Move the cursor to the first row
@@ -395,32 +234,14 @@ int main() {
                             lcd.locate(0, 1);  // Move the cursor to the second row
                             lcd.printf("Current Temp:%d", current_temp);  // Print the PWM duty cycle (add trailing spaces to clear leftovers)
                             current_temp = Read_Temperature();
-<<<<<<< HEAD
                             
                             // Update the previous values
                             previous_temp = current_temp;
                             previous_target_temp = target_temp;
                         }     
-=======
-                            printTimer.reset();
-                            }
 
->>>>>>> parent of c787c3c (henry update)
-=======
-        //             if (current_temp != previous_temp || target_temp != previous_target_rpm) {
-        //                     lcd.cls();
-        //                     wait_us(2000);
-        //                     lcd.locate(0, 0);  // Move the cursor to the first row
-        //                     lcd.printf("Target Temp:%d", target_temp);  // Print current fan RPM (add trailing spaces to clear leftovers)
-        //                     lcd.locate(0, 1);  // Move the cursor to the second row
-        //                     lcd.printf("Current Temp:%d", current_temp);  // Print the PWM duty cycle (add trailing spaces to clear leftovers)
-        //                     current_temp = Read_Temperature();
-                            
-        //                     // Update the previous values
-        //                     previous_temp = current_temp;
-        //                     previous_target_temp = target_temp;
-        //                 }     
->>>>>>> parent of cf7a566 (blah 2)
+
+
 
         //             break;
 
